f99e117201dce45bb16631fb8e470ea26e989505
slepher <cxyfree@gmail.com>
2020-06-30 22:03:31 +0800

    add macro suite

---
+++ test/astranaut_macro/astranaut_macro_SUITE.erl
@@ -0,0 +1,157 @@
+%%%-------------------------------------------------------------------
+%%% @author Chen Slepher <slepheric@gmail.com>
+%%% @copyright (C) 2018, Chen Slepher
+%%% @doc
+%%%
+%%% @end
+%%% Created :  8 Dec 2018 by Chen Slepher <slepheric@gmail.com>
+%%%-------------------------------------------------------------------
+-module(astranaut_macro_SUITE).
+
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+%%--------------------------------------------------------------------
+%% @spec suite() -> Info
+%% Info = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+suite() ->
+    [{timetrap,{seconds,30}}].
+
+%%--------------------------------------------------------------------
+%% @spec init_per_suite(Config0) ->
+%%     Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @spec end_per_suite(Config0) -> term() | {save_config,Config1}
+%% Config0 = Config1 = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+end_per_suite(_Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec init_per_group(GroupName, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% GroupName = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_group(_GroupName, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @spec end_per_group(GroupName, Config0) ->
+%%               term() | {save_config,Config1}
+%% GroupName = atom()
+%% Config0 = Config1 = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec init_per_testcase(TestCase, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% TestCase = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @spec end_per_testcase(TestCase, Config0) ->
+%%               term() | {save_config,Config1} | {fail,Reason}
+%% TestCase = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec groups() -> [Group]
+%% Group = {GroupName,Properties,GroupsAndTestCases}
+%% GroupName = atom()
+%% Properties = [parallel | sequence | Shuffle | {RepeatType,N}]
+%% GroupsAndTestCases = [Group | {group,GroupName} | TestCase]
+%% TestCase = atom()
+%% Shuffle = shuffle | {shuffle,{integer(),integer(),integer()}}
+%% RepeatType = repeat | repeat_until_all_ok | repeat_until_all_fail |
+%%              repeat_until_any_ok | repeat_until_any_fail
+%% N = integer() | forever
+%% @end
+%%--------------------------------------------------------------------
+groups() ->
+    [].
+
+%%--------------------------------------------------------------------
+%% @spec all() -> GroupsAndTestCases | {skip,Reason}
+%% GroupsAndTestCases = [{group,GroupName} | TestCase]
+%% GroupName = atom()
+%% TestCase = atom()
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+all() -> 
+    [test_macro_with_warnings, test_macro_with_vars].
+
+%%--------------------------------------------------------------------
+%% @spec TestCase() -> Info
+%% Info = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% @spec TestCase(Config0) ->
+%%               ok | exit() | {skip,Reason} | {comment,Comment} |
+%%               {save_config,Config1} | {skip_and_save,Reason,Config1}
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% Comment = term()
+%% @end
+%%--------------------------------------------------------------------
+test_macro_pt_case(_Config) ->
+    ?assertEqual({ok, ok}, astranaut_macro_with_pt:test()),
+    ?assertEqual(ok, astranaut_macro_with_pt:hello(world)),
+    ?assertEqual({error, foo}, astranaut_macro_with_pt:hello(foo)),
+    ok.
+
+test_macro_with_warnings(_Config) ->
+    Forms = astranaut_warnings:forms(),
+    [{BaseLine, _}] = astranaut:attributes_with_line(base_line, Forms),
+    [{File1, Warnings1}, {File2, Warnings2}] = astranaut_warnings:warnings(),
+    Line1 = BaseLine + 3,
+    Line2 = BaseLine + 10,
+    Line3 = BaseLine + 16,
+    Line4 = BaseLine + 18,
+    Line5 = BaseLine + 23,
+    ?assertEqual("astranaut_warnings.erl", filename:basename(File1)),
+    ?assertEqual("astranaut_warnings.erl", filename:basename(File2)),
+    ?assertEqual(
+        [{Line5, astranaut_quote,{non_empty_tail,[{atom,Line5,tail}]}}], Warnings1),
+    ?assertEqual(
+       [{Line1, astranaut_traverse, noop_function},
+        {Line2, astranaut_warnings__local_macro, noop},
+        {Line3, astranaut_warnings__local_macro, noop},
+        {Line4, astranaut_warnings__local_macro, noop}],
+       Warnings2),
+    ok.
+
+test_macro_with_vars(_Config) ->
+    Value = astranaut_macro_test:test_macro_with_vars(13),
+    ?assertEqual(112, Value).
+    
